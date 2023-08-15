30eb3d53b9bef9712c7156925fbd61d1d9ed6253
sachi <sachinnaik109@gmail.com>
2023-08-15 23:51:50 +0530

    s

--- processor.py
+++ processor.py
@@ -2,7 +2,7 @@ import re
 import pandas as pd
 
 
-def preprocessor(dat):
+def preprocessor(data):
     pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
     pattern2 = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s[APapMm]{2}\s-\s'
     pattern3 = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
