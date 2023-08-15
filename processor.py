a006227db2a94e74e4d6696de6f255359df025ed
Sachinnaik3 <98148389+Sachinnaik3@users.noreply.github.com>
2023-08-15 21:52:28 +0530

    Update processor.py

--- processor.py
+++ processor.py
@@ -26,12 +26,13 @@ def preprocessor(data):
     
     # Try both 12-hour and 24-hour formats for parsing the dates
     try:
-        df = pd.DataFrame({"user_message": massage, "date": pd.to_datetime(converted_dates, format='%m/%d/%Y, %I:%M %p - ')})
+        df = pd.DataFrame({"user_message": massage, "date": pd.to_datetime(converted_dates, format='%d/%m/%Y, %I:%M %p - ')})
     except ValueError:
         try:
-            df = pd.DataFrame({"user_message": massage, "date": pd.to_datetime(converted_dates, format='%m/%d/%Y, %I:%M %p')})
+            df = pd.DataFrame({"user_message": massage, "date": pd.to_datetime(converted_dates, format='%d/%m/%Y, %I:%M %p')})
         except ValueError:
             df = pd.DataFrame({"user_message": massage, "date": pd.to_datetime(converted_dates, format='%d/%m/%Y, %I:%M %p ')})
+            
             
     user = []
     massage = []
